e8744978844be9d53fd2c6ba4af4162d71a62294
Wevans313 <92608251+Wevans313@users.noreply.github.com>
2022-02-20 10:38:19 -0500

    Add files via upload

---
+++ Bank App/src/Bank.java
@@ -0,0 +1,20 @@
+
+public class Bank {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Account Bruce = new Account("Bruce Wayne", "BATMAN");
+		
+		
+		Account Clark = new Account("Clark Kent", "SUPERMAN");
+		
+		
+		Account Diana = new Account("Diana Prince", "WONDER WOMAN");
+		
+		
+		Account Barry = new Account("Barry Allen", "THE FLASH");
+		Barry.showMenu();
+	}
+
+}
